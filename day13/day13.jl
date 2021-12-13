99746e6c2aa364eefcfc21c9dae62212a3cdeeb6
Matthew Brookhart <mbrookhart@octoml.ai>
2021-12-13 10:01:05 -0700

    cleanup day13

--- day13/day13.jl
+++ day13/day13.jl
@@ -16,17 +16,13 @@ function load(file)
       push!(points, parse.(Int64, split(lines[i],",")))
     end
   end
-  points = hcat(points...) .+ 1 # julia is 1 indexed
-  if instructions[1][1] == 1
-    N = 2 * instructions[1][2] - 1
-    M = 2 * instructions[2][2] - 1
-  else
-    M = 2 * instructions[1][2] - 1
-    N = 2 * instructions[2][2] - 1
-  end
+  points = hcat(points...)' .+ 1 # julia is 1 indexed
+  #we always fold in half, so base the inital size on the initial instructions
+  N = 2 * filter(x->(x[1] == 0), instructions)[1][2] - 1
+  M = 2 * filter(x->(x[1] == 1), instructions)[1][2] - 1
   grid = spzeros(N, M)
-  for i in 1:size(points)[2]
-    grid[points[2, i], points[1, i]] = 1
+  for i in 1:size(points)[1]
+    grid[points[i, 1], points[i, 2]] = 1
   end
   instructions, grid .> 0
 end
@@ -34,15 +30,9 @@ end
 function fold(grid, instruction)
   N, M = size(grid)
   if instruction[1] == 1
-    left = grid[1:instruction[2] - 1,:]
-    start = instruction[2] + 1
-    right = grid[N:-1:start, :]
-    grid = left .| right
+    grid = grid[:, 1:instruction[2] - 1] .| grid[:, M:-1:instruction[2] + 1,]
   else
-    left = grid[:, 1:instruction[2] - 1]
-    start = instruction[2] + 1
-    right = grid[:,M:-1:start]
-    grid = left .| right
+    grid = grid[1:instruction[2] - 1,:] .| grid[N:-1:(instruction[2] + 1), :]
   end
   grid
 end
@@ -58,7 +48,7 @@ function problem2(A)
   for instruction  in instructions
     grid = fold(grid, instruction)
   end
-  Array(grid)
+  Array(grid')
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
